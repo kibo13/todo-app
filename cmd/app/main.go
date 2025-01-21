cad3f0cb6ba155dc6d2df5bc5004473e7136b559
kibo13 <kimboris1310@gmail.com>
2025-01-21 14:38:30 +0500

    feat: implement service and repository layers with DI and update handler initialization

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,13 +1,19 @@
 package main
 
 import (
+	"log"
+
 	"github.com/kibo13/todo-app"
 	"github.com/kibo13/todo-app/internal/handler"
-	"log"
+	"github.com/kibo13/todo-app/internal/repository"
+	"github.com/kibo13/todo-app/internal/service"
 )
 
 func main() {
-	handlers := new(handler.Handler)
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
+
 	srv := new(todo.Server)
 	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
 		log.Fatalf("Error occured while running http server: %s", err.Error())
