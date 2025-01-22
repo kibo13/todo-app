a254dad3599679c9741052d63fbdadad44595a38
kibo13 <kimboris1310@gmail.com>
2025-01-22 09:34:02 +0500

    fix: correct JSON struct tags in TodoItem fields

--- internal/entity/todo.go
+++ internal/entity/todo.go
@@ -15,8 +15,8 @@ type UserList struct {
 type TodoItem struct {
 	Id          int    `json:"id"`
 	Title       string `json:"title"`
-	Description string `json"description"`
-	Done        bool   `json"done"`
+	Description string `json:"description"`
+	Done        bool   `json:"done"`
 }
 
 type ListsItem struct {
