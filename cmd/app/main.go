d8372a5db51f21b116e2c88a06b58ccee57836bb
kibo13 <kimboris1310@gmail.com>
2025-01-22 16:15:41 +0500

    feat(config): integrate viper for configuration management

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -7,15 +7,26 @@ import (
 	"github.com/kibo13/todo-app/internal/handler"
 	"github.com/kibo13/todo-app/internal/repository"
 	"github.com/kibo13/todo-app/internal/service"
+	"github.com/spf13/viper"
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing configs: %s", err.Error())
+	}
+
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
 	srv := new(todo.Server)
-	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("8080"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("Error occured while running http server: %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("config")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
