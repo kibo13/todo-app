9329acef82e3ac0776614850792fc11d9db1acfe
kibo13 <kimboris1310@gmail.com>
2025-01-23 11:05:49 +0500

    feat(config): load environment variables using godotenv

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
-	"log"
-
+	"github.com/joho/godotenv"
 	"github.com/kibo13/todo-app"
 	"github.com/kibo13/todo-app/internal/handler"
 	"github.com/kibo13/todo-app/internal/repository"
 	"github.com/kibo13/todo-app/internal/service"
 	"github.com/spf13/viper"
+	"log"
+	"os"
 )
 
 func main() {
@@ -15,7 +16,24 @@ func main() {
 		log.Fatalf("error initializing configs: %s", err.Error())
 	}
 
-	repos := repository.NewRepository()
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("error loading env variables: %s", err.Error())
+	}
+
+	db, err := repository.NewPostgresDb(repository.Config{
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		Username: viper.GetString("db.username"),
+		DBName:   viper.GetString("db.dbname"),
+		SSLMode:  viper.GetString("db.sslmode"),
+		Password: os.Getenv("DB_PASSWORD"),
+	})
+
+	if err != nil {
+		log.Fatalf("failed to initialize db: %s", err.Error())
+	}
+
+	repos := repository.NewRepository(db)
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
