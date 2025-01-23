957e03cecb96b271168f37bf5137931d64733ba1
kibo13 <kimboris1310@gmail.com>
2025-01-23 13:15:01 +0500

    feat(auth): implement user registration functionality

--- internal/handler/auth.go
+++ internal/handler/auth.go
@@ -1,9 +1,26 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/kibo13/todo-app/internal/entity"
+)
 
 func (h *Handler) signUp(c *gin.Context) {
+	var input todo.User
 
+	if err := c.BindJSON(&input); err != nil {
+		newErrorResponse(c, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	id, err := h.services.Authorization.CreateUser(input)
+	if err != nil {
+		newErrorResponse(c, http.StatusInternalServerError, err.Error())
+	}
+
+	c.JSON(http.StatusOK, map[string]interface{}{"id": id})
 }
 
 func (h *Handler) signIn(c *gin.Context) {
