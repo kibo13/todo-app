649e2ac122b78153e4ec20dfa7d36822bbc29d94
kibo13 <kimboris1310@gmail.com>
2025-01-23 11:05:39 +0500

    feat(repository): add PostgreSQL connection using sqlx and lib/pq

---
+++ internal/repository/postgres.go
@@ -0,0 +1,33 @@
+package repository
+
+import (
+	"fmt"
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+)
+
+type Config struct {
+	Host     string
+	Port     string
+	Username string
+	Password string
+	DBName   string
+	SSLMode  string
+}
+
+func NewPostgresDb(cfg Config) (*sqlx.DB, error) {
+	connStr := fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=%s", cfg.Host, cfg.Port, cfg.Username, cfg.DBName, cfg.Password, cfg.SSLMode)
+
+	db, err := sqlx.Open("postgres", connStr)
+
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	return db, err
+}
