13991dd09f351bc3fe50e2f44015e69a672050e2
kibo13 <kimboris1310@gmail.com>
2025-01-27 16:44:45 +0500

    feat: add description field to todo_items migration

--- migrations/20250122105111_create_todo_items_table.up.sql
+++ migrations/20250122105111_create_todo_items_table.up.sql
@@ -2,5 +2,6 @@ CREATE TABLE
     todo_items (
         id serial primary key,
         title varchar(255) not null,
+        description varchar(255),
         done boolean not null default false
     );
