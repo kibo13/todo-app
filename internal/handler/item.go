a844e161d3d9011a19b523ad67bf391f07789091
kibo13 <kimboris1310@gmail.com>
2025-01-28 13:27:36 +0500

    docs(items): add Swagger annotations for CRUD operations on items

--- internal/handler/item.go
+++ internal/handler/item.go
@@ -7,6 +7,21 @@ import (
 	"strconv"
 )
 
+// @Summary Create a new todo item
+// @Security ApiKeyAuth
+// @Tags Items
+// @Description Create a new todo item in a specific list
+// @ID create-item
+// @Accept json
+// @Produce json
+// @Param id path int true "List ID"
+// @Param input body entity.TodoItem true "Todo item info"
+// @Success 200 {integer} integer "Item ID"
+// @Failure 400 {object} errorResponse "Bad Request"
+// @Failure 404 {object} errorResponse "List not found"
+// @Failure 500 {object} errorResponse "Internal Server Error"
+// @Failure default {object} errorResponse "Unexpected error"
+// @Router /api/lists/{id}/items [post]
 func (h *Handler) createItem(c *gin.Context) {
 	userId, err := getUserId(c)
 	if err != nil {
@@ -36,6 +51,20 @@ func (h *Handler) createItem(c *gin.Context) {
 	})
 }
 
+// @Summary Get all items in a todo list
+// @Security ApiKeyAuth
+// @Tags Items
+// @Description Get all todo items for a specific list
+// @ID get-all-items
+// @Accept json
+// @Produce json
+// @Param id path int true "List ID"
+// @Success 200 {array} entity.TodoItem "List of Todo items"
+// @Failure 400 {object} errorResponse "Bad Request"
+// @Failure 404 {object} errorResponse "List not found"
+// @Failure 500 {object} errorResponse "Internal Server Error"
+// @Failure default {object} errorResponse "Unexpected error"
+// @Router /api/lists/{id}/items [get]
 func (h *Handler) getAllItem(c *gin.Context) {
 	userId, err := getUserId(c)
 	if err != nil {
@@ -57,6 +86,20 @@ func (h *Handler) getAllItem(c *gin.Context) {
 	c.JSON(http.StatusOK, items)
 }
 
+// @Summary Get todo item by ID
+// @Security ApiKeyAuth
+// @Tags Items
+// @Description Get a specific todo item by its ID
+// @ID get-item-by-id
+// @Accept json
+// @Produce json
+// @Param id path int true "Item ID"
+// @Success 200 {object} entity.TodoItem "Todo item"
+// @Failure 400 {object} errorResponse "Bad Request"
+// @Failure 404 {object} errorResponse "Item not found"
+// @Failure 500 {object} errorResponse "Internal Server Error"
+// @Failure default {object} errorResponse "Unexpected error"
+// @Router /api/items/{id} [get]
 func (h *Handler) getItemById(c *gin.Context) {
 	userId, err := getUserId(c)
 	if err != nil {
@@ -78,6 +121,21 @@ func (h *Handler) getItemById(c *gin.Context) {
 	c.JSON(http.StatusOK, item)
 }
 
+// @Summary Update a todo item
+// @Security ApiKeyAuth
+// @Tags Items
+// @Description Update a specific todo item
+// @ID update-item
+// @Accept json
+// @Produce json
+// @Param id path int true "Item ID"
+// @Param input body entity.UpdateItemInput true "Updated item info"
+// @Success 200 {object} statusResponse "Status OK"
+// @Failure 400 {object} errorResponse "Bad Request"
+// @Failure 404 {object} errorResponse "Item not found"
+// @Failure 500 {object} errorResponse "Internal Server Error"
+// @Failure default {object} errorResponse "Unexpected error"
+// @Router /api/items/{id} [put]
 func (h *Handler) updateItem(c *gin.Context) {
 	userId, err := getUserId(c)
 	if err != nil {
@@ -104,6 +162,20 @@ func (h *Handler) updateItem(c *gin.Context) {
 	c.JSON(http.StatusOK, statusResponse{Status: "ok"})
 }
 
+// @Summary Delete a todo item
+// @Security ApiKeyAuth
+// @Tags Items
+// @Description Delete a specific todo item by its ID
+// @ID delete-item
+// @Accept json
+// @Produce json
+// @Param id path int true "Item ID"
+// @Success 200 {object} statusResponse "Status OK"
+// @Failure 400 {object} errorResponse "Bad Request"
+// @Failure 404 {object} errorResponse "Item not found"
+// @Failure 500 {object} errorResponse "Internal Server Error"
+// @Failure default {object} errorResponse "Unexpected error"
+// @Router /api/items/{id} [delete]
 func (h *Handler) deleteItem(c *gin.Context) {
 	userId, err := getUserId(c)
 	if err != nil {
